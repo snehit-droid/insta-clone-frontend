8b615aeb3a7a94daa50aafd0cc8a7e1e4d9285d3
Snehit <snehitchats@gmail.com>
2021-02-07 15:51:30 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, createContext, useReducer, useContext } from 'react';
+import Navbar from './components/Navbar';
+import "./App.css";
+import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom';
+import Home from './components/screens/Home';
+import Signin from './components/screens/Signin';
+import Profile from './components/screens/Profile';
+import Signup from './components/screens/Signup';
+import CreatePost from './components/screens/Createpost';
+import {reducer, initialState} from './reducers/userReducer';
+import UserProfile from './components/screens/UserProfile';
+import SubscribedUserPosts from './components/screens/SubscribeUserPosts';
+
+export const UserContext = createContext()
+
+const Routing = () => {
+  const history = useHistory();
+  const {state, dispatch} = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch({type: "USER", payload: user})
+    } else {
+      history.push('/signin')
+    }
+  }, [])
+
+  return(
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/signin" exact>
+        <Signin />
+      </Route>
+      <Route path="/signup" exact>
+        <Signup />
+      </Route>
+      <Route path="/profile" exact>
+        <Profile />
+      </Route>
+      <Route path="/create" exact>
+        <CreatePost />
+      </Route>
+      <Route path="/profile/:userid" exact>
+        <UserProfile />
+      </Route>
+      <Route path="/myfollowingpost" exact>
+        <SubscribedUserPosts />
+      </Route>
+    </Switch>
+  )
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{state, dispatch}}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/screens/Profile.js
@@ -0,0 +1,109 @@
+import React, {useEffect, useState, useContext} from 'react';
+import {UserContext} from '../../App';
+
+const Profile = () => {
+    const [pics, setPics] = useState([])
+    const {state, dispatch} = useContext(UserContext);
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:5000/mypost", {
+            headers: {
+                "Authorization": "Bearer " +localStorage.getItem("jwt")
+            }
+        })
+        .then(res => res.json())
+        .then(result => {
+            setPics(result.mypost)
+        })
+    }, [])
+
+    useEffect(() => {
+        if(image){
+            const data = new FormData()
+            data.append("file", image)
+            data.append("upload_preset", "insta-clone")
+            data.append("cloud_name", "snehit-droid")
+            fetch("https://api.cloudinary.com/v1_1/snehit-droid/image/upload", {
+                method: "POST",
+                body: data,
+            })
+            .then(res => res.json())
+            .then(data => {
+                fetch('http://localhost:5000/updatepic', {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " +localStorage.getItem("jwt")
+                    },
+                    body: JSON.stringify({
+                        pic: data.url
+                    }) 
+                })
+                .then(res => res.json())
+                .then(result => {
+                    console.log(result);
+                    localStorage.setItem("user", JSON.stringify({...state, pic: result.pic}))
+                    dispatch({type: "UPDATEPIC", payload: result.pic})
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }, [image])
+
+    const updatePhoto = (file) => {
+        setImage(file)
+    }
+
+    return (
+        <div style={{maxWidth:"850px", margin:"0px auto"}}>
+            <div style={{
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+            }}>
+                <div>
+                    <img style={{width:"160px", height:"160px", borderRadius:"80px"}} 
+                    src={state ? state.pic : "loading"}
+                    />
+                </div>    
+                <div>
+                    <h4>{state ? state.name : "loading..."}</h4>
+                    <h5>{state ? state.email : "loading..."}</h5>
+                    <div style={{display:"flex", justifyContent: "space-between", width: "108%"}}>
+                        <h6>{pics.length} posts</h6>
+                        <h6>{state ? state.followers.length : "0"} followers</h6>
+                        <h6>{state ? state.following.length : "0"} following</h6>
+                    </div>
+                </div>
+                <div className="file-field input-field" style={{margin: "10px"}}>
+                <div className="btn #64b5f6 blue darken-1">
+                    <span>Upload Pic</span>
+                    <input type="file" onChange={(e) => updatePhoto(e.target.files[0])} />
+                </div>
+                <div className="file-path-wrapper">
+                    <input className="file-path validate" type="text" />
+                </div>
+               </div>
+            </div>
+            </div>
+            
+            <div className="gallery">
+                {
+                    pics.map(item => {
+                        return(
+                            <img key={item._id} className="item" src={item.photo} alt={item.title} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Profile
